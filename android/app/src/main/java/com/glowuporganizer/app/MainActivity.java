13ca4f0e3267a71073eb0601626db13db618f19a
Zakays <mariagildagoncalves3793@gmail.com>
2026-01-16 00:21:04 -0300

    feat: Update Android configuration for development - WebView debugging, Java 21, and cleartext traffic

--- android/app/src/main/java/com/glowuporganizer/app/MainActivity.java
+++ android/app/src/main/java/com/glowuporganizer/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.glowuporganizer.app;
 
 import com.getcapacitor.BridgeActivity;
+import android.webkit.WebView;
+import android.os.Build;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Enable WebView debugging for development
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
